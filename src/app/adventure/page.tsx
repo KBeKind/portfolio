e69ad766b3b8632e7b58b69b73f22f54cb5134a8
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-08-21 15:39:08 -0500

    adding basic structure and outline

---
+++ src/app/adventure/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <p>Kyle's Mystical Adventure</p>
+
+      <p>KMA is:...</p>
+    </>
+  );
+}
---
+++ src/app/gardenparty/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <p>Garden Party</p>
+
+      <p>Garden Party is:...</p>
+    </>
+  );
+}
