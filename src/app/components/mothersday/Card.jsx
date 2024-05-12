4dad83f043e9c17a64ff62700833eb989ccfad02
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-05-12 18:26:16 -0500

    commit

---
+++ src/app/components/mothersday/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="bg-indigo-600 p-5 rounded-md  m-4">
+        You are correct!
+        <br />
+        Sandy Toepfer is the best mom ever!
+      </div>
+      <div className="bg-indigo-600 p-5 rounded-md m-4">
+        HAPPY MOTHER'S DAY!
+        <br />
+        I love you Mom!
+        <br />
+        Thank you for all the support and fun times.
+      </div>
+    </div>
+  );
+};
+
+export default Card;
